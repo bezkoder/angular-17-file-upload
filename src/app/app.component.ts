03c8fe6d7b37abb292c36191af8c38d0918100d2
tienbku <tien.ngtruong@gmail.com>
2023-12-26 14:24:21 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FileUploadComponent } from './components/file-upload/file-upload.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FileUploadComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-17-file-upload';
+  title = 'Angular 17 File Upload example';
 }
---
+++ src/app/components/file-upload/file-upload.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { FileUploadService } from '../../services/file-upload.service';
+
+@Component({
+  selector: 'app-file-upload',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './file-upload.component.html',
+  styleUrl: './file-upload.component.css',
+})
+export class FileUploadComponent implements OnInit {
+  currentFile?: File;
+  message = '';
+  fileInfos?: Observable<any>;
+
+  constructor(private uploadService: FileUploadService) {}
+
+  ngOnInit(): void {
+    this.fileInfos = this.uploadService.getFiles();
+  }
+
+  selectFile(event: any): void {
+    this.message = '';
+    this.currentFile = event.target.files.item(0);
+  }
+
+  upload(): void {
+    if (this.currentFile) {
+      this.uploadService.upload(this.currentFile).subscribe({
+        next: (event: any) => {
+          if (event instanceof HttpResponse) {
+            this.message = event.body.message;
+            this.fileInfos = this.uploadService.getFiles();
+          }
+        },
+        error: (err: any) => {
+          console.log(err);
+
+          if (err.error && err.error.message) {
+            this.message = err.error.message;
+          } else {
+            this.message = 'Could not upload the file!';
+          }
+        },
+        complete: () => {
+          this.currentFile = undefined;
+        },
+      });
+    }
+  }
+}
